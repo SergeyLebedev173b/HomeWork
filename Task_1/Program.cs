6e7a1fc2fed55244e1b76cea0ba1958d1757ace6
sergeylebedev <lebedevs06@gmail.com>
2022-10-15 19:48:50 +0400

    Нахождение большего из двух чисел

---
+++ Task_1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите часло А");
+int namber_1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите часло B");
+int namber_2 = Convert.ToInt32(Console.ReadLine());
+int max = namber_1;
+
+if(namber_1 > namber_2)
+{
+    Console.WriteLine(namber_1);
+}
+else
+{
+   Console.WriteLine(namber_2);
+}
